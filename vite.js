fe7f6f91e249f373e010b4eb6166ba89e36acfd2
Jonathan Martinez Gutierrez <utm180149ti@ut-morelia.edu.mx>
2024-08-20 22:15:25 -0600

    ner

---
+++ vite.js
@@ -0,0 +1,9 @@
+// vite.config.js
+export default {
+    server: {
+      port: 3000, // Puerto en el que corre el servidor de desarrollo
+    },
+    build: {
+      outDir: 'dist', // Directorio de salida para los archivos construidos
+    },
+  };
